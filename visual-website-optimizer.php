c1f7b9333ec7bf46111dee67e80d87c042c9bb6d
teamplaylotto <teamplaylotto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-19 13:04:15 +0000

    change to use website in names and refs instead of web (branding)
    git-svn-id: https://plugins.svn.wordpress.org/visual-web-optimizer/trunk@219546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- visual-website-optimizer.php
+++ visual-website-optimizer.php
@@ -4,7 +4,7 @@ Plugin Name: Visual Website Optimizer
 Plugin URI: http://comluv.com
 Description: Visual Website Optimizer is the world's easiest to use A/B, split and multivariate testing tool. Simply enable the plugin and start running tests on your Wordpress website without doing any other code changes. Visit <a href="http://visualwebsiteoptimizer.com/">Visual Website Optimizer</a> for more details.
 Author: Andy Bailey
-Version: 1.0
+Version: 1.0.1
 Author URI: http://fiddyp.co.uk
 
 This relies on the actions being present in the themes header.php and footer.php
